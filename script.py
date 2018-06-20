8b74168b4a5c356cca963bd76a8782afbb99b2b1
voidiker66 <asi327@utexas.edu>
2018-06-20 14:49:35 -0700

    remove script

--- script.py
+++
@@ -1,4 +0,0 @@
-from random import randint
-
-with open("uint_test.py", 'w') as f:
-	f.write("from io       import StringIO\nfrom unittest import main, TestCase\nfrom Collatz import collatz_read, collatz_eval, collatz_print, collatz_solve\n\n")
